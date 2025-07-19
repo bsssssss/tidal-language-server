276a768a58a2fd663dae17361ffbd8daf96ba54b
Bss <mx.bss@proton.me>
2025-07-20 01:59:28 +0200

    docs: add comments
    fix: remove '|' character at beginning of parsed tidal functions
    documentation

--- src/Tidal/Documentation.hs
+++ src/Tidal/Documentation.hs
@@ -26,18 +26,21 @@ import           System.Environment          (getEnv)
 import           System.FilePath
 
 
+-- | Information about a Tidal function including its name, documentation, and type signature
 data FunctionInfo = FunctionInfo
-    { functionName :: String
-    , functionDocs :: String
-    , functionType :: String
+    { functionName :: String -- ^ The function name
+    , functionDocs :: String -- ^ The Haddock documentation
+    , functionType :: String -- ^ The formatted type signature
     } deriving (Show, Eq)
 
+-- | Get the Tidal source directory from the TIDAL_SRC_PATH environment variable
 getTidalSrcDir :: IO (Maybe FilePath)
 getTidalSrcDir = catch (Just . (</> "tidal-core/src/Sound/Tidal") <$> getEnv "TIDAL_SRC_PATH") handleMissingEnv
     where
         handleMissingEnv :: IOException -> IO (Maybe FilePath)
         handleMissingEnv _ = return Nothing
 
+-- | Resolve a Tidal filename to its full path
 resolveTidalFile :: FilePath -> IO FilePath
 resolveTidalFile filename = do
     maybeDir <- getTidalSrcDir
@@ -45,6 +48,7 @@ resolveTidalFile filename = do
         Just dir -> return (dir </> filename)
         Nothing  -> error "TIDAL_SRC_PATH not in PATH"
 
+-- | Get all Tidal source files, excluding certain utility files
 getTidalSourceFiles :: IO [FilePath]
 getTidalSourceFiles = do
     maybeDir <- getTidalSrcDir
@@ -56,6 +60,7 @@ getTidalSourceFiles = do
             return $ map (\f -> dir </> f) selectedFiles
         Nothing -> return []
 
+-- | Language extensions needed to parse Tidal source files
 tidalExtensions :: [Extension]
 tidalExtensions =
     [ EnableExtension ExplicitForAll
@@ -67,18 +72,21 @@ tidalExtensions =
     , EnableExtension InstanceSigs
     ]
 
+-- | Global TVar containing cached Tidal documentation
 tidalDocsVar :: TVar [FunctionInfo]
 tidalDocsVar = unsafePerformIO $ do
     docs <- collectDocumentation
     newTVarIO docs
 {-# NOINLINE tidalDocsVar #-}
 
+-- | Collect documentation from all Tidal source files
 collectDocumentation :: IO [FunctionInfo]
 collectDocumentation = do
     files <- getTidalSourceFiles
     functionInfos <- mapM collectFunctionsFromFile files
     return (concat functionInfos)
 
+-- | Parse a Haskell file and extract function documentation
 collectFunctionsFromFile :: FilePath -> IO [FunctionInfo]
 collectFunctionsFromFile path = do
     -- putStrLn $ "Parsing -> " ++ path
@@ -108,24 +116,28 @@ toFunctionInfo (TypeSig (_, comments) names type') = do
         formatDocs :: [Comment] -> String
         formatDocs = unlines . map (cleanComment . (\(Comment _ _ txt) -> txt))
         cleanComment txt = case dropWhile isSpace txt of
-            -- '|':rest -> dropWhile isSpace rest
+            '|':rest -> dropWhile isSpace rest
             '>':rest -> dropWhile isSpace rest
             other    -> other
 
 toFunctionInfo _ = Nothing
 
+-- | Normalize whitespace in a string by collapsing multiple spaces
 normalizeWhitespace :: String -> String
 normalizeWhitespace = unwords . words
 
+-- | Find documentation for a specific Tidal function by name
 findTidalFunction :: String -> IO (Maybe FunctionInfo)
 findTidalFunction funcName = do
     -- docs <- collectDocumentation
     docs <- readTVarIO tidalDocsVar
     return $ lookupFunction funcName docs
 
+-- | Lookup a function in a list of FunctionInfo by name
 lookupFunction :: String -> [FunctionInfo] -> Maybe FunctionInfo
 lookupFunction funcName = find (\doc -> functionName doc == funcName)
 
+-- | Format function information for display in LSP hover
 formatTidalFunction :: FunctionInfo -> Text
 formatTidalFunction FunctionInfo{..} = T.pack $
             functionType ++ "\n" ++
@@ -137,6 +149,7 @@ printFunctionInfo FunctionInfo {..} = do
     putStrLn $ functionName ++ " :: " ++ functionType
     putStrLn $ unlines $ map ("  " ++) $ lines functionDocs
 
+-- | Test function to find and print documentation for a function
 testFindFunction :: String -> IO ()
 testFindFunction funcName = do
     result <- findTidalFunction funcName
@@ -144,6 +157,7 @@ testFindFunction funcName = do
         Just functionInfo -> printFunctionInfo functionInfo
         Nothing -> putStrLn $ "Function '" ++ funcName ++ "' not found !"
 
+-- | Test function to extract and display documentation from a specific file
 testPath :: FilePath -> IO ()
 testPath filename = do
     path <- case filename of
